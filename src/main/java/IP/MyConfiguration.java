48bf615c61d0ed914a3e4b15db8b95a8d9535535
dianamocanu05 <diana.paula.mocanu@gmail.com>
2021-05-23 14:34:29 +0300

    cors config

--- src/main/java/IP/MyConfiguration.java
+++ src/main/java/IP/MyConfiguration.java
@@ -2,23 +2,25 @@ package IP;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
-public class MyConfiguration extends WebSecurityConfigurerAdapter {
+public class MyConfiguration {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+                registry.addMapping("/*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
+                        .allowedHeaders("")
                         .allowedOrigins("*");
 
 
             }
         };
     }
+
 }
